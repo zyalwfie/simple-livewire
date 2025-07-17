0f8a8423bb98b3860d07f7c21d4ed4dce929d1d5
Ziyad Alwafie <zyalwfie@gmail.com>
2025-07-17 22:46:08 +0800

    livewire-sandbox: #3 actions

--- app/Livewire/Users.php
+++ app/Livewire/Users.php
@@ -4,6 +4,8 @@ namespace App\Livewire;
 
 use App\Models\User;
 use Livewire\Component;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 
 class Users extends Component
 {
@@ -12,6 +14,17 @@ class Users extends Component
     public function __construct()
     {
         $this->users = User::all();
+    }
+
+    public function generateRandomUser()
+    {
+        User::create([
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'remember_token' => Str::random(10),
+        ]);
     }
 
     public function render()
--- resources/views/livewire/users.blade.php
+++ resources/views/livewire/users.blade.php
@@ -1,6 +1,10 @@
 <div class="max-w-3xl h-screen flex items-center justify-center flex-col gap-4 mx-auto">
     <h1 class="text-center text-3xl font-bold">Users List ({{ count($users) }})</h1>
-    <ul class="flex flex-col items-center gap-4">
+    <div class="flex gap-2 items-center">
+        <button type="button" wire:click="generateRandomUser" class="px-4 py-2 rounded border border-gray-300 bg-gray-300 text-gray-800 transition hover:bg-gray-600 hover:text-gray-200 cursor-pointer">Create new random user</button>
+            <button type="button" wire:click="$refresh" class="px-4 py-2 rounded border border-blue-50 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 cursor-pointer">Reload</button>
+    </div>
+    <ul class="flex flex-col items-center gap-4 max-h-50 overflow-auto">
         @foreach ($users as $user)
             <li>
                 <span>{{ $user->name }}</span>
